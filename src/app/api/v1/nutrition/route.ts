1901aac46c11db0f0e571d370396580119c14c39
Pony <pony076152340@gmail.com>
2025-02-20 18:54:58 +0800

    bug: Fix get nutrition overview api bug

--- src/app/api/v1/nutrition/route.ts
+++ src/app/api/v1/nutrition/route.ts
@@ -1,80 +1,68 @@
 import { type NextRequest } from "next/server";
 import {
-  checkIsPayloadValidOrThrowRequestError,
-  checkIsRequestAuthorizedOrThrowError,
-  checkIsRequestBodyJsonOrThrowRequestError,
-  checkIsRequestError,
-  RequestErrorType,
-} from "@/utils/api";
-import {
   GetNutritionOverRequestSchema,
   GetNutritionOverResponseSchema,
 } from "@/schema/nutrition-schema";
-import { PrismaClient, Food } from "@prisma/client";
+import { Food } from "@prisma/client";
 import { z } from "zod";
+import { ApiHandler } from "@/lib/server/api/handler";
+
+type FoodWithIntake = Food & { intake: number };
 
 export const GET = async (request: NextRequest) => {
-  try {
-    const authTokenPayload = checkIsRequestAuthorizedOrThrowError(request);
-    const searchParams = Object.fromEntries(
-      request.nextUrl.searchParams.entries()
-    );
-    const validPayload = checkIsPayloadValidOrThrowRequestError(
-      GetNutritionOverRequestSchema,
-      searchParams
-    );
-
-    const { date } = validPayload;
-    const dbClient = new PrismaClient();
-
-    const selectDate = new Date(date);
-    const meals = await dbClient.meal.findMany({
-      include: {
-        foods: true,
-      },
-      where: {
-        userId: authTokenPayload.id,
-        date: {
-          gte: new Date(selectDate.setHours(0, 0, 0, 0)), // 今天 00:00:00
-          lte: new Date(selectDate.setHours(23, 59, 59, 999)), // 今天 23:59:59.999
+  const handler = new ApiHandler(
+    GetNutritionOverRequestSchema,
+    GetNutritionOverResponseSchema,
+    true,
+    async ({ request, prisma, validatedPayload, authPayload }) => {
+      const { date } = validatedPayload;
+      const selectDate = new Date(date);
+      const meals = await prisma.meal.findMany({
+        select: {
+          MealItem: {
+            select: {
+              intake: true,
+              food: true,
+            },
+          },
         },
-      },
-    });
+        where: {
+          userId: authPayload.id,
+          date: {
+            gte: new Date(selectDate.setHours(0, 0, 0, 0)), // 今天 00:00:00
+            lte: new Date(selectDate.setHours(23, 59, 59, 999)), // 今天 23:59:59.999
+          },
+        },
+      });
 
-    const foodList = meals.reduce((acc, meal) => {
-      return acc.concat(meal.foods);
-    }, [] as Food[]);
+      const foodList = meals.reduce((acc, meal) => {
+        return acc.concat(
+          meal.MealItem.map((item) => ({ ...item.food, intake: item.intake }))
+        );
+      }, [] as FoodWithIntake[]);
 
-    const nutritionOverview = getNutritionOverviewByFoodList(foodList);
+      const nutritionOverview = getNutritionOverviewByFoodList(foodList);
 
-    const responsePayload = GetNutritionOverResponseSchema.parse({
-      nutrition: nutritionOverview,
-    });
-
-    return new Response(JSON.stringify(responsePayload), { status: 200 });
-  } catch (err) {
-    if (checkIsRequestError(err)) {
-      switch (err.error.type) {
-        case RequestErrorType.NOT_AUTHORIZED:
-          return new Response("Not Authorized", { status: 403 });
-        case RequestErrorType.INVALID_REQUEST_PAYLOAD:
-          return new Response("Invalid Request Payload", { status: 400 });
-      }
+      return { nutrition: nutritionOverview };
     }
-    return new Response("Unknown Error", { status: 500 });
-  }
+  );
+
+  return handler.handle(request);
 };
+
 type OverviewPayload = z.infer<
   typeof GetNutritionOverResponseSchema
 >["nutrition"];
 
-const getNutritionOverviewByFoodList = (foodList: Food[]): OverviewPayload => {
+const getNutritionOverviewByFoodList = (
+  foodList: FoodWithIntake[]
+): OverviewPayload => {
   return foodList.reduce(
     (acc, food) => {
-      acc.calories += food.calories;
-      acc.protein += food.protein;
-      acc.carbs += food.carbs;
-      acc.fat += food.fat;
+      acc.calories += food.calories * food.intake;
+      acc.protein += food.protein * food.intake;
+      acc.carbs += food.carbs * food.intake;
+      acc.fat += food.fat * food.intake;
       return acc;
     },
     { calories: 0, protein: 0, carbs: 0, fat: 0 } as OverviewPayload
