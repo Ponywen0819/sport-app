6ac3a427c9c0fb1126916031755bb782d8f51f26
Pony <pony076152340@gmail.com>
2026-05-09 21:40:19 +0800

    Feat: remove all and leave refactor doc

--- src/app/(date)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from "react";
-
-const Layout = ({ children }: PropsWithChildren) => <>{children}</>;
-
-export default Layout;
--- src/lib/notion/client.ts
+++
@@ -1,5 +0,0 @@
-import { Client } from "@notionhq/client";
-
-export const createNotionClient = (accessToken: string): Client => {
-  return new Client({ auth: accessToken });
-};
